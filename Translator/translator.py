03cf69901aa5dddbd59ea783efe586df6e566bb3
Ushio-Hayase <ushiohayase@gmail.com>
2024-08-04 13:55:07 +0900

    30 epochs train

--- Translator/translator.py
+++ Translator/translator.py
@@ -12,7 +12,7 @@ class Translator():
         self.transformer = transformer
         transformer.eval()
 
-    def __call__(self, sentence, max_length=64):
+    def __call__(self, sentence):
 
         sentence = tokenizers.encode(sentence, padding="max_length",add_special_tokens=False,  max_length=MAX_LEN,truncation=True, return_tensors="pt")
 
@@ -30,7 +30,7 @@ class Translator():
         for _ in range(MAX_LEN-1):
             predictions = self.transformer(encoder_input, output)
             # select the last token from the seq_len dimension
-            predictions = torch.nn.functional.softmax(predictions, dim=1)  # (batch_size, vocab_size, 1)
+            predictions = torch.nn.functional.log_softmax(predictions, dim=1)  # (batch_size, vocab_size, 1)
             predictions = predictions[:, :, -1:]
             predicted_id = torch.argmax(predictions, axis=1)
 
@@ -55,12 +55,12 @@ if __name__ == "__main__":
 
     with torch.no_grad():
         model = Transformer(256 , 512, tokenizers.vocab_size, MAX_LEN, device).to(device=device)
-        model.load_state_dict(torch.load("./model-10.pt"))
+        model.load_state_dict(torch.load("./Translator/model-30.pt"))
         model.eval()
 
         ts = Translator(tokenizers, model)
 
-        sentence = "오늘은 토요일이다"
+        sentence = input("입력 >> ")
         
         text = ts(sentence)
 
